de92c35d14f27bab63b872c6dd4732276355079b
KeithSloan <keith@sloan-home.co.uk>
2020-03-23 15:52:31 +0000

    Initial Commit

---
+++ freecad/importNURBS/InitGui.py
@@ -0,0 +1,6 @@
+__title__ = "ImportNURBS"
+__author__ = "Keith Sloan (KeithSloan52)"
+__license__ = "LGPL 2.1"
+__doc__ = "FreeCAD workbench to add importer of 3DM Files"
+
+FreeCAD.addImportType("3DM (*.3dm)","import3DM")
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+__version__ = '0.1'
+
+import os
+
+def mysetup(requires) :
+   setup(name='ImportNURBS_Workbench',
+      version=str(__version__),
+      packages=['freecad','rhino3dm']
+      maintainer="keithsloan52",
+      maintainer_email="keith@sloan-home.co.uk",
+      url="https://github.com/KeithSloan/ImportNURBS",
+      description="Workbench to add support for import of 3DM Files to FreeCAD",
+      install_requires=[requires],
+      include_package_data=True
+      include_package_data=True)
+
+# Still not clear if under linux one can just install lxml with pip
+# or sudo apt-get install python3-lxml
+
+mysetup('rhino3dm')
+#import os
+#if 'posix' in os.name:
+#    mysetup('lxml')
+#
+#else:
+#    mysetup('python3-lxml')
